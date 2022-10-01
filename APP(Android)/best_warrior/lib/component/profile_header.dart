de4456a9c0faf81a351663e9c716a272631a6e80
ZeVicTech <113289544+ZeVicTech@users.noreply.github.com>
2022-10-01 14:46:40 +0000

    Make Component

---
+++ APP(Android)/best_warrior/lib/component/profile_header.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+
+class ProfileHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        _buildHeaderProfile()
+      ],
+    );
+  }
+
+  Widget _buildHeaderProfile() {
+    return Column(
+      // ignore: prefer_const_literals_to_create_immutables
+      children: [
+        Text(
+          '이정호',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+        ),
+        Text(
+          '2탄약중대/상병',
+          style: TextStyle(fontSize: 30),
+        ),
+        Text(
+          '특기: 체력',
+          style: TextStyle(fontSize: 15),
+        ),
+      ],
+    );
+  }
+}
