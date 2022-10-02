b6922a9712c5ef207739f6c97fb1850812a1c044
yong <75594904+umdeukyong@users.noreply.github.com>
2022-10-02 14:55:27 +0000

    firebase 연동

--- APP(Android)/best_warrior/lib/main.dart
+++ APP(Android)/best_warrior/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:best_warrior/component/profile_header.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MyApp());
 }
 
