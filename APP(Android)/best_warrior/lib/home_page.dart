6e477e8e73102c33be60df18842b47982a64b2f4
yong <75594904+umdeukyong@users.noreply.github.com>
2022-10-04 14:08:33 +0000

    login_page기본 틀 개발

--- APP(Android)/best_warrior/lib/home_page.dart
+++ APP(Android)/best_warrior/lib/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'component/auth.dart';
+import 'package:best_warrior/component/auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
--- APP(Android)/best_warrior/lib/main.dart
+++ APP(Android)/best_warrior/lib/main.dart
@@ -2,31 +2,25 @@ import 'package:best_warrior/component/profile_header.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:best_warrior/component/widget_tree.dart';
 
-void main() {
+Future<void> main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ProfilePage(),
-    );
-  }
-}
-
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          ProfileHeader(),
-        ],
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
       ),
+      home: const WidgetTree(),
     );
   }
-}
+} 
