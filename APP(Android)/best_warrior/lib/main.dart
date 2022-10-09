c283917009291ce04ad75272bfbd25cde64e0fec
yong <75594904+umdeukyong@users.noreply.github.com>
2022-10-09 08:29:04 +0000

    커밋테스트

--- APP(Android)/best_warrior/lib/main.dart
+++ APP(Android)/best_warrior/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.blue,
       ),
       home: const WidgetTree(),
     );
