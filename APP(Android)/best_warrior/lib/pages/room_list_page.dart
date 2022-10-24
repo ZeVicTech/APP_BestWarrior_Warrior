492f7581df403b5afc2e3991520f8f948751216e
ZeVicTech <113289544+ZeVicTech@users.noreply.github.com>
2022-10-24 15:00:55 +0000

    룸 리스트 구현

--- APP(Android)/best_warrior/lib/pages/room_list_page.dart
+++ APP(Android)/best_warrior/lib/pages/room_list_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -11,14 +12,57 @@ class RoomListPage extends StatefulWidget {
 }
 
 class _RoomListPageState extends State<RoomListPage> {
+
+  int? selectedIndex;
+  List<Tech> _chipsList = [
+    Tech("Android", Colors.green),
+    Tech("Flutter", Colors.blueGrey),
+    Tech("Ios", Colors.deepOrange),
+    Tech("Python", Colors.cyan),
+    Tech("Go lang", Colors.yellow)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      child: Text(
-        'RoomListPage',
-        style: TextStyle(fontSize: 30),
-      ),
+      child: Wrap(
+        spacing: 6,
+        direction: Axis.horizontal,
+        children: techChips(),
+      )
     );
   }
+
+  List<Widget> techChips () {
+    List<Widget> chips = [];
+    for (int i=0; i< _chipsList.length; i++) {
+      Widget item = Padding(
+        padding: const EdgeInsets.only(left:10, right: 5),
+        child: ChoiceChip(
+          label: Text(_chipsList[i].label),
+          labelStyle: TextStyle(color: Colors.white),
+          backgroundColor: _chipsList[i].color,
+          selected: selectedIndex == i,
+          onSelected: (bool value)
+          {
+            setState(() {
+              selectedIndex = i;
+            });
+          },
+        ),
+      );
+      chips.add(item);
+    }
+    return chips;
+  }
 }
+
+class Tech
+{
+  String label;
+  Color color;
+  Tech(this.label, this.color);
+}
+
+
